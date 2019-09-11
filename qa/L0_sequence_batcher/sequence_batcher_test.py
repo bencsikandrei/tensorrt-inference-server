117921b4fab60fae30d9c8f47dabd4311bcc8d0e
Tanmay Verma <tanmay2592@gmail.com>
2019-09-11 15:09:33 -0700

    Changes to include supported TRT IO datatypes in testing (#646)
    * Include other trt datatypes
    
    * fixing the tuple representation

--- qa/L0_sequence_batcher/sequence_batcher_test.py
+++ qa/L0_sequence_batcher/sequence_batcher_test.py
@@ -95,10 +95,9 @@ class SequenceBatcherTest(unittest.TestCase):
 
         if (("savedmodel" in trial) or ("graphdef" in trial) or
             ("netdef" in trial) or ("custom" in trial) or
-            ("onnx" in trial) or ("libtorch" in trial)):
+            ("onnx" in trial) or ("libtorch" in trial) or 
+	        ("plan" in trial)):
             tensor_shape = (1,)
-        elif "plan" in trial:
-            tensor_shape = (1,1,1)
         else:
             self.assertFalse(True, "unknown trial type: " + trial)
 
@@ -245,10 +244,9 @@ class SequenceBatcherTest(unittest.TestCase):
 
         if (("savedmodel" in trial) or ("graphdef" in trial) or
             ("netdef" in trial) or ("custom" in trial) or
-            ("onnx" in trial) or ("libtorch" in trial)):
+            ("onnx" in trial) or ("libtorch" in trial) or
+            ("plan" in trial)):
             tensor_shape = (1,)
-        elif "plan" in trial:
-            tensor_shape = (1,1,1)
         else:
             self.assertFalse(True, "unknown trial type: " + trial)
 
